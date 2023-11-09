12b666d32a437b7e2da31b6fba4fcff7d57ee196
ahmedzehry55 <ahmedalhlwany2111@gmail.com>
2023-11-09 16:26:42 +0200

    'blog'

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import "./article.css"
-function Article() {
+function Article({ imgUrl, date, title}) {
   return (
-    <div>article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p><a className='subtext__color'>Read Full Article</a></p>
+      </div>
+    </div>
   )
 }
 
